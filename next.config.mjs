3fa23ad2e802da1b28c4f365a47322621b9ef8ce
yoonkyung <kyung3098@naver.com>
2024-09-07 20:37:19 +0900

    프로젝트 초기 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
+  },
+};
 
 export default nextConfig;

64ecc02bd8557b49033e98e533302893a2d2a962
yoonkyung <kyung3098@naver.com>
2024-09-09 22:03:19 +0900

    모바일, 태블릿 화면 LNB 컴포넌트 생성

---
+++ src/app/_component/LocalNavbar.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+
+import cn from 'classnames';
+import styles from './LocalNavbar.module.scss';
+import { LuDog, LuUser } from 'react-icons/lu';
+import { LuPencil } from 'react-icons/lu';
+import { MdOutlineQuiz } from 'react-icons/md';
+
+export const LocalNavbar = () => {
+  return (
+    <nav className={cn(`${styles.lnb}`, 'lg-hidden')}>
+      <ul className={styles['lnb-list']}>
+        <li className={styles['lnb-item']}>
+          <Link href="/">
+            <LuDog className={styles.icon} />
+            임보 동물
+          </Link>
+        </li>
+        <li className={styles['lnb-item']}>
+          <Link href="/">
+            <LuPencil className={styles.icon} />
+            임보 일기
+          </Link>
+        </li>
+        <li className={styles['lnb-item']}>
+          <Link href="/" className={styles['title']}></Link>
+        </li>
+        <li className={styles['lnb-item']}>
+          <Link href="/">
+            <MdOutlineQuiz className={styles.icon} />
+            임보 유형
+          </Link>
+        </li>
+        <li className={styles['lnb-item']}>
+          <Link href="/auth/login">
+            <LuUser className={styles.icon} />내 프로필
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};

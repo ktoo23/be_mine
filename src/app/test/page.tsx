5659b33c2fd40d8b85cb3a4b14faed9fdbc0713e
yoonkyung <kyung3098@naver.com>
2024-09-15 21:06:24 +0900

    🚧 SubTitle 반응형 css 추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,11 +2,12 @@ import { SubTitle } from '../_component/SubTitle';
 import { Question } from './_component/Question';
 
 import styles from './page.module.scss';
+import subTitleStyles from '../_component/subTitle.module.scss';
 
 const Page = () => {
   return (
     <>
-      <div className={styles['title-wrapper']}>
+      <div className={subTitleStyles['title-wrapper']}>
         <SubTitle title="임보 유형 테스트" />
       </div>
       <div className={styles.container}>

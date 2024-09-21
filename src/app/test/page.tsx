002e997589dd8751e3f7102e0e29a1febd6c54d0
yoonkyung <kyung3098@naver.com>
2024-09-22 02:15:06 +0900

    ♻️ 임보 테스트, 폼 업로드 페이지 컨테이너 css 공통으로 사용

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,7 @@
 import { SubTitle } from '../_component/SubTitle';
 import { Question } from './_component/Question';
 
-import styles from './page.module.scss';
+import containerStyles from '@/app/_component/containerStyle.module.scss';
 import subTitleStyles from '../_component/subTitle.module.scss';
 
 const Page = () => {
@@ -10,7 +10,7 @@ const Page = () => {
       <div className={subTitleStyles['title-wrapper']}>
         <SubTitle title="임보 유형 테스트" />
       </div>
-      <div className={styles.container}>
+      <div className={containerStyles.container}>
         <Question />
       </div>
     </>

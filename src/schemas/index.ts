16d968b8d0983dc4602689a180d1126cb9f2f432
yoonkyung <kyung3098@naver.com>
2024-09-26 17:20:13 +0900

    스키마 이름 변경

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod';
 
-export const LoginSchema = z.object({
+export const SigninSchema = z.object({
   email: z.string().email({ message: '이메일 형식이 올바르지 않습니다.' }),
   password: z.string().trim().min(1, { message: '비밀번호를 입력해주세요.' }),
 });

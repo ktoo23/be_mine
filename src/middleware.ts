adc3ae61366a195d7201d188aedf8edb984b169a
yoonkyung <kyung3098@naver.com>
2024-10-04 23:51:25 +0900

    로그인 안 한 사용자는 게시글 작성 못하게 middleware로 로그인 페이지 이동 시킴

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,13 @@
-export { auth as middleware } from '@/auth';
+import { auth } from '@/auth';
+import { NextResponse } from 'next/server';
+
+export async function middleware() {
+  const session = await auth();
+  console.log('SESSION::', session);
+  if (!session) {
+    return NextResponse.redirect('http://localhost:3000/login');
+  }
+}
 
 export const config = {
   matcher: ['/foster/write', '/diary/write'],

10e80e67ebd473826233ad46c1115378e1825719
yoonkyung <kyung3098@naver.com>
2025-03-21 22:00:17 +0900

    msw 프로덕션에서 활성화

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -2,5 +2,17 @@ import { setupWorker } from 'msw/browser';
 import { handlers } from './handlers';
 
 const worker = setupWorker(...handlers);
+if (typeof window !== 'undefined') {
+  // 프로덕션 환경에서도 MSW 활성화
+  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+    worker
+      .start({
+        onUnhandledRequest: 'bypass',
+      })
+      .catch((error) => {
+        console.error('MSW 시작 오류:', error);
+      });
+  }
+}
 
 export default worker;
